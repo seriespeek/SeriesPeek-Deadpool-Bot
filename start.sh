1040c23552500d1524dbb9b8ef3cd7d0b45bff09
seriespeek <seodurgeshsharma@gmail.com>
2024-07-15 00:09:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/seriespeek/SeriesPeek-Deadpool-Bot /SeriesPeek-Deadpool-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /SeriesPeek-Deadpool-Bot
 fi
-cd /Advance-File-Share-Bot
+cd /SeriesPeek-Deadpool-Bot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
